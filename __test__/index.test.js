f04c5d090f3c50825910feb42ba150c23ee56367
JuanSlaterT <arevalobernaljuan@gmail.com>
2024-07-09 22:26:37 -0500

    added gitignore

---
+++ __test__/index.test.js
@@ -0,0 +1,10 @@
+const request = require("supertest");
+const app = require("../app");
+
+describe("Initializing unit tests", () => {
+    it("GET /", async () => {
+        const response = await request(app).get("/")
+        console.log(response)
+        expect(response.statusCode).toBe(200)
+    })
+})
---
+++ server.js
@@ -0,0 +1,21 @@
+
+const app = require('./app');
+
+const http = require('http');
+
+const PORT = process.env.PORT || 3000;
+
+app.set('port', PORT)
+
+const server = http.createServer(app)
+
+server.listen(PORT)
+
+server.on('listening', onListening);
+
+
+function onListening() {
+
+    console.log(`Escuchando en el puerto ${PORT}`)
+
+}
