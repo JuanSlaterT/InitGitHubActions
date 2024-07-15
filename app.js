751d63b999122d3702d521677be5878a9aac1129
JuanSlaterT <arevalobernaljuan@gmail.com>
2024-07-15 11:16:09 -0500

    push para la prueba

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('¡Prueba para el enunciado de Pipelines!')
 })
 
 app.use(express.json())
